0fc371017ff0e3566ed547e6131a1201f19fd26b
Christian Alexander Wolf <wolf@auxmoney.com>
2017-08-11 23:18:56 +0200

    added extension, enabled it in testapp

---
+++ spec/ServiceContainer/AnalysisExtensionSpec.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace spec\Cawolf\Behat\Analysis\ServiceContainer;
+
+use Behat\Behat\Tester\ServiceContainer\TesterExtension;
+use Behat\Testwork\Cli\ServiceContainer\CliExtension;
+use Behat\Testwork\ServiceContainer\Extension;
+use Cawolf\Behat\Analysis\Controller\Output;
+use Cawolf\Behat\Analysis\ServiceContainer\AnalysisExtension;
+use Cawolf\Behat\Analysis\Tester\Suite;
+use Cawolf\Behat\Analysis\Timing\Accumulator;
+use PhpSpec\ObjectBehavior;
+use PHPUnit\Framework\Assert;
+use Prophecy\Argument;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+
+class AnalysisExtensionSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(AnalysisExtension::class);
+        $this->shouldHaveType(Extension::class);
+    }
+
+    function it_is_named_analysis()
+    {
+        $this->getConfigKey()->shouldReturn('analysis');
+    }
+
+    function it_is_configurable(ArrayNodeDefinition $builder)
+    {
+        $this->configure($builder);
+    }
+
+    function it_loads_all_services(ContainerBuilder $container)
+    {
+        $this->setDefinitionShouldBeCalled($container, Accumulator::SERVICE_ID, Accumulator::class);
+        $this->setDefinitionShouldBeCalled($container, TesterExtension::SUITE_TESTER_WRAPPER_TAG . Suite::SERVICE_SUFFIX, Suite::class);
+        $this->setDefinitionShouldBeCalled($container, CliExtension::CONTROLLER_TAG . Output::SERVICE_SUFFIX, Output::class);
+
+        $this->load($container, []);
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     * @param string $serviceId
+     * @param string $className
+     */
+    private function setDefinitionShouldBeCalled(ContainerBuilder $container, $serviceId, $className)
+    {
+        $definition = Argument::that(
+            function (Definition $definition) use ($className) {
+                Assert::assertEquals(
+                    $className,
+                    $definition->getClass(),
+                    sprintf(
+                        'expected %s, got %s',
+                        $className,
+                        $definition->getClass()
+                    )
+                );
+                return true;
+            }
+        );
+        $container->setDefinition($serviceId, $definition)->shouldBeCalled();
+    }
+}
---
+++ src/ServiceContainer/AnalysisExtension.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Cawolf\Behat\Analysis\ServiceContainer;
+
+use Behat\Behat\Tester\ServiceContainer\TesterExtension;
+use Behat\Testwork\Cli\ServiceContainer\CliExtension;
+use Behat\Testwork\ServiceContainer\Extension;
+use Behat\Testwork\ServiceContainer\ExtensionManager;
+use Cawolf\Behat\Analysis\Controller\Output;
+use Cawolf\Behat\Analysis\Tester\Suite;
+use Cawolf\Behat\Analysis\Timing\Accumulator;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * Main extension class, initializes wrappers.
+ */
+class AnalysisExtension implements Extension
+{
+    /** @inheritdoc */
+    public function process(ContainerBuilder $container)
+    {
+    }
+
+    /** @inheritdoc */
+    public function getConfigKey()
+    {
+        return 'analysis';
+    }
+
+    /** @inheritdoc */
+    public function initialize(ExtensionManager $extensionManager)
+    {
+    }
+
+    /** @inheritdoc */
+    public function configure(ArrayNodeDefinition $builder)
+    {
+        // TODO: make precision configurable
+    }
+
+    /** @inheritdoc */
+    public function load(ContainerBuilder $container, array $config)
+    {
+        $this->loadTimings($container);
+        $this->loadTesters($container);
+        $this->loadControllers($container);
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     */
+    private function loadTimings(ContainerBuilder $container)
+    {
+        $definition = new Definition(Accumulator::class);
+        $container->setDefinition(Accumulator::SERVICE_ID, $definition);
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     */
+    private function loadTesters(ContainerBuilder $container)
+    {
+        $definition = new Definition(
+            Suite::class, [new Reference(TesterExtension::SUITE_TESTER_ID), new Reference(Accumulator::SERVICE_ID)]
+        );
+        $definition->addTag(TesterExtension::SUITE_TESTER_WRAPPER_TAG, ['priority' => 10000]);
+        $container->setDefinition(
+            TesterExtension::SUITE_TESTER_WRAPPER_TAG . Suite::SERVICE_SUFFIX,
+            $definition
+        );
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     */
+    private function loadControllers(ContainerBuilder $container)
+    {
+        $definition = new Definition(Output::class);
+        $definition->addTag(CliExtension::CONTROLLER_TAG, ['priority' => 100]);
+        $container->setDefinition(
+            CliExtension::CONTROLLER_TAG . Output::SERVICE_SUFFIX,
+            $definition
+        );
+    }
+}
